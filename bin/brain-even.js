c712c028759cbbd6a44ff4df9ee8fc1204eb42f7
Victoria <vict.lebedeva@gmail.com>
2022-08-30 21:41:27 +0300

    modify bin/brain-even.js extracting common logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import brainEvenGame from '../games/games-even';
 
-const checkEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
-
-const brainEvenGame = (name) => {
-  const num = Math.round(Math.random() * 100);
-  console.log(`Question: ${num}`);
-  const answer = readlineSync.question('Your answer: ');
-  const corrAnswer = checkEven(num);
-  if (corrAnswer === answer) {
-    console.log('Correct!');
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${corrAnswer}.`);
-    console.log(`Let's try again, ${name}!`);
-  }
-  return corrAnswer === answer;
-};
-
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
-let success = 0;
-while (success < 3) {
-  success = brainEvenGame(name) ? success + 1 : 4;
-  if (success === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-}
+brainEvenGame();
