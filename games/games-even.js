5d1ee5ea63c6d5cb0deca0d7db0dcc49d00e83d4
Victoria <vict.lebedeva@gmail.com>
2022-08-30 21:43:13 +0300

    add games/games-even.js unique game description

---
+++ games/games-even.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { isCorrect, greeting } from '../src/index.js';
+
+const checkEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+const brainEvenRound = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const num = Math.round(Math.random() * 100);
+  console.log(`Question: ${num}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const corrAnswer = checkEven(num);
+  return [userAnswer, corrAnswer];
+};
+
+const brainEvenGame = () => {
+  const name = greeting();
+  for (let i = 0; i < 3; i += 1) {
+    if (!isCorrect(brainEvenRound(), name)) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default brainEvenGame;
