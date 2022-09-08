76a2b4cf3c0d1922219f81d2f565530639c0ebb2
Victoria <vict.lebedeva@gmail.com>
2022-09-08 22:30:50 +0300

    modify src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { isCorrect, greeting } from '../index.js';
-
-const generateRand = (minNum, maxNum) => {
-  const min = Math.ceil(minNum);
-  const max = Math.floor(maxNum);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
-};
+import runGame from '../index.js';
+import generateRand from '../utils.js';
 
 const generateProgr = (length, startElem, interval) => {
   const progr = [];
@@ -17,7 +12,7 @@ const generateProgr = (length, startElem, interval) => {
   return progr;
 };
 
-const brainProgressionRound = () => {
+const runRound = () => {
   const length = generateRand(5, 10); // длина прогрессии от 5 до 10
   const replElemNum = generateRand(0, length - 1); // номер загадываемого элемента
   const startElem = generateRand(0, 100); // значение начального элемента
@@ -31,14 +26,8 @@ const brainProgressionRound = () => {
   return [userAnswer, corrAnswer];
 };
 
-const brainProgressionGame = () => {
-  const name = greeting();
-  for (let i = 0; i < 3; i += 1) {
-    if (!isCorrect(brainProgressionRound(), name)) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+const runBrainProgression = () => {
+  runGame(runRound);
 };
 
-export default brainProgressionGame;
+export default runBrainProgression;
