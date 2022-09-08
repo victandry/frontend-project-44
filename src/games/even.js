30d6d6d8fd64c0f859e52a660ae29d74036eaa5d
Victoria <vict.lebedeva@gmail.com>
2022-09-08 22:29:57 +0300

    modify src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { isCorrect, greeting } from '../index.js';
+import runGame from '../index.js';
+import generateRand from '../utils.js';
 
 const checkEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
-const brainEvenRound = () => {
+const runRound = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const num = Math.round(Math.random() * 100);
+  const num = generateRand(0, 100);
   console.log(`Question: ${num}`);
   const userAnswer = readlineSync.question('Your answer: ');
   const corrAnswer = checkEven(num);
   return [userAnswer, corrAnswer];
 };
 
-const brainEvenGame = () => {
-  const name = greeting();
-  for (let i = 0; i < 3; i += 1) {
-    if (!isCorrect(brainEvenRound(), name)) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+const runBrainEven = () => {
+  runGame(runRound);
 };
 
-export default brainEvenGame;
+export default runBrainEven;
