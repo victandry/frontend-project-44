e1f8d44b58db61915f7f4530bfeb575ad0fc04d2
Victoria <vict.lebedeva@gmail.com>
2022-09-08 22:30:33 +0300

    modify src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { isCorrect, greeting } from '../index.js';
-
-const generateRand = () => (Math.round(Math.random() * 100));
+import runGame from '../index.js';
+import generateRand from '../utils.js';
 
 const isPrime = (num) => {
   let div = 1;
@@ -16,23 +15,17 @@ const isPrime = (num) => {
   return answer;
 };
 
-const brainPrimeRound = () => {
+const runRound = () => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const num = generateRand();
+  const num = generateRand(0, 100);
   console.log(`Question: ${num}`);
   const userAnswer = readlineSync.question('Your answer: ');
   const corrAnswer = isPrime(num);
   return [userAnswer, corrAnswer];
 };
 
-const brainPrimeGame = () => {
-  const name = greeting();
-  for (let i = 0; i < 3; i += 1) {
-    if (!isCorrect(brainPrimeRound(), name)) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+const runBrainPrime = () => {
+  runGame(runRound);
 };
 
-export default brainPrimeGame;
+export default runBrainPrime;
