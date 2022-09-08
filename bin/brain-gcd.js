0f4a057f022e10278145eb7bd116ebacc1cb3c7c
Victoria <vict.lebedeva@gmail.com>
2022-09-08 22:31:52 +0300

    modify bin/brain-calc.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import brainGcdGame from '../src/games/gcd.js';
+import runBrainGcd from '../src/games/gcd.js';
 
-brainGcdGame();
+runBrainGcd();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import brainProgressionGame from '../src/games/progression.js';
+import runBrainProgression from '../src/games/progression.js';
 
-brainProgressionGame();
+runBrainProgression();
