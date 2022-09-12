238bc0e54424a97619f36eeb622fb00961f31565
Victoria <vict.lebedeva@gmail.com>
2022-09-12 23:51:52 +0300

    modify src/utils.js renaming fucntions

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-const generateRand = (minNum, maxNum) => {
-  const min = Math.ceil(minNum);
-  const max = Math.floor(maxNum);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
-};
+const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 
-export default generateRand;
+export default getRandomNumber;
