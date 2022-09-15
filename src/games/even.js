a15f8ca673c8e49cd8006c396cb8408584759183
Victoria <vict.lebedeva@gmail.com>
2022-09-15 23:13:59 +0300

    modify src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
 import run from '../index.js';
-import getRandomNumber from '../utils.js';
+import { getRandomNumber } from '../utils.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minValue = 0;
+const maxValue = 100;
 
 const checkEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 const runRound = () => {
-  const randomNumber = getRandomNumber(0, 100);
-  const question = `Question: ${randomNumber}`;
+  const randomNumber = getRandomNumber(minValue, maxValue);
+  const question = `${randomNumber}`;
   const correctAnswer = checkEven(randomNumber);
   return [question, correctAnswer];
 };
 
 const runEven = () => {
-  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-  run(runRound, gameDescription);
+  run(runRound, description);
 };
 
 export default runEven;
