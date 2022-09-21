dd0b9a652f5238c9df4bb3cc72fd6b4481b32781
Victoria <vict.lebedeva@gmail.com>
2022-09-22 01:13:37 +0300

    modify src/utils.js renaming function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 
-export const generateIndex = (array) => getRandomNumber(0, array.length - 1);
+export const getRandomIndex = (array) => getRandomNumber(0, array.length - 1);
