71c7fea2a2cab8ff3ae24c7b35cad1d53fa83fa2
Victoria <vict.lebedeva@gmail.com>
2022-09-22 01:14:26 +0300

    modify src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
 import run from '../index.js';
-import { getRandomNumber, generateIndex } from '../utils.js';
+import { getRandomNumber, getRandomIndex } from '../utils.js';
 
 const description = 'What is the result of the expression?';
 const minValue = 0;
 const maxValue = 100;
-
-const getRandomOperator = () => {
-  const operators = ['+', '-', '*'];
-  const index = generateIndex(operators); // для выбора рандомного операнда
-  return operators[index];
-};
+const operators = ['+', '-', '*'];
 
 const calculate = (x, y, operator) => {
   switch (operator) {
@@ -22,17 +17,17 @@ const calculate = (x, y, operator) => {
   }
 };
 
-const runRound = () => {
+const getRound = () => {
   const number1 = getRandomNumber(minValue, maxValue);
   const number2 = getRandomNumber(minValue, maxValue);
-  const operator = getRandomOperator();
+  const operator = operators[getRandomIndex(operators)];
   const question = `${number1} ${operator} ${number2}`;
   const correctAnswer = String(calculate(number1, number2, operator));
   return [question, correctAnswer];
 };
 
 const runCalc = () => {
-  run(runRound, description);
+  run(getRound, description);
 };
 
 export default runCalc;
