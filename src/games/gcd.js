c2a1aaae06b0d736f81bb29de0d551e9a84cc907
Victoria <vict.lebedeva@gmail.com>
2022-09-22 01:15:57 +0300

    modify src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,28 +7,9 @@ const description = 'Find the greatest common divisor of given numbers.';
 const minValue = 0;
 const maxValue = 100;
 
-const getGcd = (number1, number2) => {
-  let gcd = 1; // НОД по умолчанию
-  let remainder = 2; // остаток от деления, сначала произвольный
-  let x = Math.abs(number1);
-  let y = Math.abs(number2);
-  if (Math.sign(x - y) < 0) {
-    x = y;
-    y = Math.abs(number1);
-  }
-  while (remainder ** 2 > 1) { // алгоритм Евклида
-    remainder = x % y; // если = 0, то НОД = absnum2; если = 1, то вз-простые числа
-    gcd = y;
-    x = y;
-    y = remainder;
-    if (remainder === 1) {
-      gcd = 1;
-    }
-  }
-  return gcd;
-};
+const getGcd = (x, y) => (x % y === 0 ? y : getGcd(y, x % y));
 
-const runRound = () => {
+const getRound = () => {
   const number1 = getRandomNumber(minValue, maxValue);
   const number2 = getRandomNumber(minValue, maxValue);
   const question = `${number1} ${number2}`;
@@ -37,7 +18,7 @@ const runRound = () => {
 };
 
 const runGcd = () => {
-  run(runRound, description);
+  run(getRound, description);
 };
 
 export default runGcd;
